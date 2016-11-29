fa762839cac2a52fbec73bbfd75d00f763dac7c9
tastybento <tastybento@wasteofplastic.com>
2016-11-28 22:11:01 -0800

    Removed debug.

--- src/com/wasteofplastic/askygrid/generators/WorldStyles.java
+++ src/com/wasteofplastic/askygrid/generators/WorldStyles.java
@@ -125,7 +125,7 @@ public class WorldStyles {
 		s.add(type);
 	    }
 	}
-	Bukkit.getLogger().info("DEBUG: spawner list = " + s);
+	//Bukkit.getLogger().info("DEBUG: spawner list = " + s);
 	return s;
     }
 
